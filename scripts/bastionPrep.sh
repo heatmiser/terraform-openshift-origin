9cbf651050aca68cd9f77b822e8b88002ee2520d
Mike Savage <mike.savage@gmail.com>
2018-01-12 11:57:26 -0500

    first commit

---
+++ scripts/bastionPrep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo $(date) " - Starting Bastion Prep Script"
+
+# Install base packages and update system to latest packages
+echo $(date) " - Install base packages and update system to latest packages"
+
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools kexec-tools sos psacct
+yum -y update --exclude=WALinuxAgent
+yum -7 install centos-release-openshift-origin37.noarch
+
+# Install OpenShift utilities
+echo $(date) " - Installing OpenShift utilities"
+
+yum -y install atomic-openshift-utils
+
+# Create playbook to update ansible.cfg file to include path to library
+
+cat > updateansiblecfg.yaml <<EOF
+#!/usr/bin/ansible-playbook
+
+- hosts: localhost
+  gather_facts: no
+  tasks:
+  - lineinfile:
+      dest: /etc/ansible/ansible.cfg
+      regexp: '^library '
+      insertafter: '#library        = /usr/share/my_modules/'
+      line: 'library = /usr/share/ansible/openshift-ansible/library/'
+EOF
+
+# Run Ansible Playbook to update ansible.cfg file
+
+echo $(date) " - Updating ansible.cfg file"
+
+ansible-playbook ./updateansiblecfg.yaml
+
+echo $(date) " - Script Complete"
